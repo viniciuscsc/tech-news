d801b4247d8688c21b3f167b449045dc496f05d0
viniciuscsc <viniciuscsc@icloud.com>
2023-11-13 12:31:11 -0300

    Req 08: Cria a função search_by_date()

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -15,8 +16,21 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        date_iso = datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    date_db = date_iso.strftime("%d/%m/%Y")
+
+    query = {"timestamp": date_db}
+
+    noticias = []
+
+    for noticia in search_news(query):
+        noticias.append((noticia["title"], noticia["url"]))
+
+    return noticias
 
 
 # Requisito 9
